30da1e0d90c2a1e684f1e82cb81e5a831d335c6f
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-05-26 15:11:54 -0600

    Finished the design of the DashboardLayout. Added component SidebarMenuItem

--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -8,7 +8,8 @@ import ProsConsStreamPage from "../pages/pros-cons-stream/ProsConsStreamPage";
 import ProsConsPage from "../pages/pros-cons/ProsConsPage";
 import TextToAudioPage from "../pages/text-to-audio/TextToAudioPage";
 import TranslatePage from "../pages/translate/TranslatePage";
-import DashboardLayout from "../layouts/DashboardLayout";
+// import DashboardLayout from "../layouts/DashboardLayout";
+import { DashboardLayout } from "../layouts/DashboardLayout";
 
 export const menuRoutes = [
   {
