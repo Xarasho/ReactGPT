f1083c9fe0dbd7258ebe921fe7426e5152e56053
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-05-26 18:42:05 -0600

    Added the GptMessage and MyMessage components to OrthographyPage

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,1 +1,6 @@
-export * from './sidebar/SidebarMenuItem'
+export * from './chat-bubbles/GptMessage';
+export * from './chat-bubbles/MyMessage';
+
+export * from './loaders/TypingLoader';
+
+export * from './sidebar/SidebarMenuItem';
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,6 +1,22 @@
+import { GptMessage, MyMessage, TypingLoader } from '../../components';
 const OrthographyPage = () => {
   return (
-    <div>Orthography</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+          {/* Bienvenida */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones." />
+
+          <MyMessage text="Hola Mundo" />
+
+          <div className='fade-in'>
+            <TypingLoader />
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
